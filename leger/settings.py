fad81a77998c954b99a4f493918de19e8c803c6d
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2023-08-31 01:13:10 -0600

    heroku commit2

--- leger/settings.py
+++ leger/settings.py
@@ -171,9 +171,9 @@ AWS_S3_OBJECT_PARAMETERS = {
   }
 AWS_LOCATION = 'ledger_stat'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-#STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
