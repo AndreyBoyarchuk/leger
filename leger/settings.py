4dcba826f8f55d52d129d0accab14dc7009c0738
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2023-08-31 00:18:29 -0600

    heroku commit2

--- leger/settings.py
+++ leger/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ledgernest-c805af24d555.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['ledgernest-c805af24d555.herokuapp.com','127.0.0.1','web-production-4926.up.railway.app','www.ledgernest.us','www.ledgernest.com']
 
 # Application definition
 
