a32a2f09ecb43b8bba1df41a95bee86ebe1b305c
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2023-08-30 18:10:11 -0600

    replit commit

--- reports/models.py
+++ reports/models.py
@@ -11,3 +11,24 @@ class TextContent(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.category})"
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    index = models.IntegerField()
+    date = models.DateField()
+    account_number = models.IntegerField()
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    debit = models.DecimalField(max_digits=20, decimal_places=2)
+    credit = models.DecimalField(max_digits=20, decimal_places=2)
+    source_account_number = models.IntegerField()
+    created_at = models.DateTimeField()
+    memo = models.TextField()
+    source_account = models.TextField()
+    payee = models.TextField()
+    customer = models.TextField()
+    category_account = models.TextField()
+    account = models.TextField()
+    subcategory = models.TextField()
+    project = models.TextField()
+
+    def __str__(self):
+        return f"Transaction {self.id}"
